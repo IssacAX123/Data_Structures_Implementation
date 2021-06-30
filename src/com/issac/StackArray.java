5ddde63c25907c44a2dcaf44a76f32d0061bb01f
Issac Abraham <issaca32@gmail.com>
2021-06-30 13:02:31 +0100

    adds pushes to top of stack method that also raises exceptions

--- src/com/issac/StackArray.java
+++ src/com/issac/StackArray.java
@@ -1,4 +1,25 @@
 package com.issac;
 
 public class StackArray {
+    int size;
+    int top = -1;
+    int[] stack;
+
+    public StackArray(int size){
+        stack = new int[size];
+    }
+    public boolean isFull(){
+        return top == stack.length - 1;
+    }
+    public boolean isEmpty(){
+        return top == 0;
+    }
+
+    public void push(int data) throws Exception {
+        if(!isFull()) {
+            stack[top] = data;
+        }else{
+            throw new Exception("Stack capacity is full");
+        }
+    }
 }
