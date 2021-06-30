d4937ffd7f6a97f7144a0f4f8055d7bd27bbb972
Issac Abraham <issaca32@gmail.com>
2021-06-30 12:15:25 +0100

    creates isFull method to check if mac capacity has been met

--- src/com/issac/DynamicArray.java
+++ src/com/issac/DynamicArray.java
@@ -1,10 +1,15 @@
 package com.issac;
 
 public class DynamicArray {
+    int rear_pointer;
     int[] dynamicArray;
 
     public DynamicArray(){
+        rear_pointer = 0;
         dynamicArray = new int[4];
     }
 
+    public boolean isFull(){
+        return rear_pointer == dynamicArray.length;
+    }
 }
